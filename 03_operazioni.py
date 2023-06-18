83d76b48b281d76830314daf658904255bde5863
Pierporz <82719938+peiro91@users.noreply.github.com>
2023-06-18 17:20:38 +0200

    Create 03_operazioni.py

---
+++ 03_operazioni.py
@@ -0,0 +1,37 @@
+numero1 = 11
+numero2 = 4
+
+
+somma = numero1 + numero2
+sottrazione = numero1 - numero2
+moltiplicazione = numero1 * numero2
+divisione = numero1 / numero2
+potenza = numero1 ** numero2
+div_intera = numero1 // numero2
+modulo = numero1 % numero2
+
+print("La somma è:" ,somma)
+print("La sottrazione è:" ,sottrazione)
+print("La moltiplicazione è:" ,moltiplicazione)
+print("La divisione è:" ,divisione)
+print("La potenza è:" ,potenza)
+print("La div_intera è:" ,div_intera)
+print("La modulo è:" ,modulo)
+
+print(20-6*2)
+print((20-6)*2)
+
+parte1 = "Ciao mi chiamo "
+parte2 = "Mario Rossi"
+messaggio_intero = parte1 + parte2
+print(messaggio_intero)
+
+numero3 = "7"
+numero4 = "8"
+somma_stringa = numero3 + numero4
+
+print(somma_stringa)
+
+canale_yt = "Pierporz"
+moltiplicazione_stringa = canale_yt * numero1
+print(moltiplicazione_stringa)
