4ba26376a459b1d345110cac7bc8feee67d37bf5
Pierporz <82719938+peiro91@users.noreply.github.com>
2023-06-18 13:56:55 +0200

    Create 02_variabili.py

---
+++ 02_variabili.py
@@ -0,0 +1,16 @@
+nome = "Mario Rossi"
+altezza = 180
+indossa_occhiali = False
+
+print(nome,altezza,indossa_occhiali)
+
+nome = "Anna Verdi"
+altezza = 165
+indossa_occhiali = True
+
+print(nome,altezza,indossa_occhiali)
+
+print(type(nome))
+print(type(altezza))
+print(type(indossa_occhiali))
+print(type(3.14))
